08b17838a1fe9863c0b61740b15531a2d79f32bf
Matteo Repetto <matteo.repetto@ge.imati.cnr.it>
2022-05-30 10:22:31 +0200

    Added placeholder for simulator.sh

---
+++ simulator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in `seq 1 10`; do
+	echo "***********************"
+	echo "I'm only a placeholder!"
+	echo "Please replace me with "
+	echo "meaningful software!!!!"
+	echo "***********************"
+	
+	sleep 10;
+done
